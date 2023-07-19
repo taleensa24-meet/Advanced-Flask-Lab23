eda5bb6f3c1346aa0cef001d2e88b30b3007b58c
taleensa24-meet <139681215+taleensa24-meet@users.noreply.github.com>
2023-07-19 13:50:31 +0300

    Adv

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -25,7 +25,8 @@ posts = {
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html')
+    return render_template('index.html',im=image_link,us=user_bio, pos=posts)
+
 
 
 @app.route('/about')  # '/' for the default page
